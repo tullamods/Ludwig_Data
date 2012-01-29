0d33b1780d8fb4fef529c2c0ccd5bc0e4cd935cf
Jaliborc <contact@jaliborc.com>
2012-01-29 20:44:13 +0000

    Added the crawler

---
+++ crawler.py
@@ -0,0 +1,174 @@
+﻿import urllib2, re, time
+
+# Localizations
+TargetFile = '/Users/Jaliborc/Addons/LudwigItems.lua'
+ItemSearchURL = 'http://www.wowhead.com/items?filter=cr=151:151;crs=4:1;crv='
+ClassSearchURL = 'http://static.wowhead.com/js/locale_enus.js?1241'
+
+# Values
+Markers = {1:'{', 2:'}', 3:'$', 4:'€', 5:'£'}
+NumQualities = 7
+MaxItems = 80000
+MinItems = 0
+ItemBump = 50
+Verbose = None
+
+# Globals
+Start = time.time()
+Cache = {}
+Dir = type(Cache)
+String = type('')
+items = '[['
+classes = '[['
+
+
+# Cache Methods
+def SetTable(table, index):
+    table[index] = table.get(index) or {}
+
+def AddItem(table, quality, level, item):
+    table[level] = table.get(level) or {}
+    level = table[level]
+    
+    old = level.get(quality) or ''
+    level[quality] = old + item
+
+# Item Matching Methods
+def GetValue(key, kind, text):
+    match = re.search(key + '":' + kind, text)
+    if match:
+        return match.group(1)
+
+def GetNumber(key, text):
+    return GetValue(key, '(-*\d+)', text)
+
+
+# Browse Items
+for value in range(MinItems, MaxItems, ItemBump):
+    upper = str(value + ItemBump - 1)
+    lower = str(value)
+
+    if Verbose:
+        print('')
+        
+    print('Searching from ' + lower + ' to ' + upper)
+    
+    url = ItemSearchURL + upper + ':' + lower
+    page = urllib2.urlopen(url)
+    source = page.read()
+    
+    for match in re.finditer('"classs".*?cost', source):
+        full = match.group(0)
+
+        itemID = GetNumber('"id', full)
+        name = re.sub('\\\\', '', GetValue('name', '"\d(.+?[^\\\])"', full))
+        itemData = itemID + ';' + name + ';'
+
+        itemClass = GetNumber('classs', full) or 'None'
+        subClass = GetNumber('subclass', full) or 'None'
+        slot = GetNumber('slot', full) or 'None'
+
+        quality = NumQualities - int(GetValue('name', '["\'](\d)', full))
+        level = GetNumber('reqlevel', full)
+        level = level and int(level) or 'None'
+
+        if Verbose:
+            print(itemData, level)
+            
+        SetTable(Cache, itemClass)
+        SetTable(Cache[itemClass], subClass)
+        SetTable(Cache[itemClass][subClass], slot)
+        AddItem(Cache[itemClass][subClass][slot], quality, level, itemData)
+
+
+# Browse Categories
+print('')
+print('Browsing Category Names...')
+page = urllib2.urlopen(ClassSearchURL)
+source = re.search('var mn_items=(.*?);var', page.read()).group(1)
+ids = [None, 0, 0, 0]
+level = 0
+
+for match in re.finditer('\[([^\[]*)', source):
+    section = match.group(1)
+    level = level + 1
+
+    if level < 7:
+        classs = re.search('^(-*\d+),"([^"]+)"', section)
+
+        if classs:
+            classID = classs.group(1)
+            className = classs.group(2)
+            classLevel = level / 2
+
+            if Verbose:
+                print(className, classID, classLevel)
+
+            parentCache = Cache
+            
+            for i in range(1, classLevel):
+                parentCache = parentCache.get(ids[i])
+
+                if not parentCache:
+                    break
+
+            for i in range(classLevel + 1, 4):
+                ids[i] = 0
+
+            if parentCache and parentCache.get(classID):
+                newID = ids[classLevel] + 1
+                ids[classLevel] = newID
+                
+                classes = classes + Markers[classLevel] + className
+                parentCache[newID] = parentCache[classID]
+                parentCache[classID] = None
+
+    level = level - len(re.findall('\]', section))
+
+
+# Extract Cache
+def ExtractValue(table, level, marker, i):
+    global items
+    value = table.get(i)
+    
+    if value and not value == None:
+        if not type(i) == String:
+            items = items + str(i) + marker
+
+        if type(value) == Dir:
+            Extract(value, level)
+        else:
+            items = items + value
+
+def Extract(table, level):
+    marker = Markers[level]
+    level = level + 1
+    done = {}
+    i = 1
+
+    while table.get(i):
+        ExtractValue(table, level, marker, i)
+        done[i] = True
+        i = i + 1
+        
+    for i, value in table.items():
+        if not done.get(i):
+            ExtractValue(table, level, marker, i)
+
+print('Extracting Cache...')
+Extract(Cache, 1)
+
+
+# Write the File
+f = open(TargetFile, 'w')
+f.write('Ludwig_Classes=' + classes + ']]\n\nLudwig_Items='+ items +']]')
+f.close()
+
+took = time.time() - Start
+hours = round(took / 3600)
+minutes = round((took - hours) / 60)
+seconds = took - hours - minutes
+
+print('')
+print('Done!')
+print('Took ' + str(hours) + ' hours ' + str(minutes) + ' minutes ' + str(seconds) + ' seconds')
