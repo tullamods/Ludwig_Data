0c4db0b43dd0c83c42ac36d11fcf29a7acdb1340
Jaliborc <contact@jaliborc.com>
2013-11-24 20:01:08 +0000

    New number encoding. Large crawler refactoring.

--- api.py
+++ api.py
@@ -1,8 +1,34 @@
-import requests, re, json
+# -*- coding: utf-8 -*-
+import requests, re
+
+Markers = {1:'¤', 2:'¢', 3:'€', 4:'£', 5:'฿'}
+Alphabet = [chr(i) for i in xrange(0, 127)]
+NumQualities = 7
 
 # Web
+def GetItems(url, pages):
+    items = []
+
+    for i, page in enumerate(pages[:-1]):
+        source = GetWebPage(url + str(pages[i+1] - 1) + ':' + str(page))
+        matches = re.findall('"classs".*?cost', source)
+        print('Found ' + str(len(matches)) + ' items')
+
+        for match in matches:
+            items += [{
+                'id': GetNumber('"id', match),
+                'class': GetNumber('classs', match),
+                'subclass': GetNumber('subclass', match),
+                'slot': GetNumber('slot', match),
+
+                'name': re.sub('\\\\', '', GetValue('name', '"\d(.+?[^\\\])"', match)),
+                'quality': NumQualities - int(GetValue('name', '["\'](\d)', match)),
+                'level': int(GetNumber('reqlevel', match))
+            }]
+
+    return items
+
 def GetClasses(url):
-    print('Browsing Item Classes...')
     source = GetWebPage(url)
     data = re.search('var mn_items=(.*?);\s*var', source, re.DOTALL).group(1)
     classes = []
@@ -27,7 +53,8 @@ def GetClasses(url):
     return classes
         
 def GetWebPage(url):
-	while True:
+    print('Retrieving ' + url)
+    while True:
 		page = requests.get(url)
 		
 		try:
@@ -37,24 +64,91 @@ def GetWebPage(url):
 			print('Error retrieving page, repeating')
 			continue
 
+def GetNumber(key, text):
+    return GetValue(key, '(-*\d+)', text) or 0
 
-# Caching
-def SetTable(table, index):
-    table[index] = table.get(index) or {}
-
-def AddItem(table, quality, level, item):
-    table[level] = table.get(level) or {}
-    level = table[level]
-    
-    old = level.get(quality) or ''
-    level[quality] = old + item
-
-
-# Parsing
 def GetValue(key, kind, text):
     match = re.search(key + '":' + kind, text)
     if match:
         return match.group(1)
 
-def GetNumber(key, text):
-    return GetValue(key, '(-*\d+)', text)
+
+# Hierarchization
+def Hierarchize(items):
+    table = {}
+    
+    for item in items:   
+        SetTable(table, item['class'])
+        SetTable(table[item['class']], item['subclass'])
+        SetTable(table[item['class']][item['subclass']], item['slot'])
+        SetTable(table[item['class']][item['subclass']][item['slot']], item['level'])
+        AddItem(table[item['class']][item['subclass']][item['slot']][item['level']], item['quality'], CompressInt(item['id'], 3) + item['name'] + ';')
+        
+    return table
+
+def SetTable(table, index):
+    table[index] = table.get(index) or {}
+
+def AddItem(table, quality, item):
+    table.setdefault(quality, '')
+    table[quality] += item
+    
+    
+# Class IDs
+def GenerateClassIDs(classes, items):
+    ids = [None, 0, 0, 0]
+    record = ''
+
+    for c in classes:
+        table = items
+
+        for i in xrange(1, c['level']):
+            table = table.get(ids[i])
+            if not table:
+                break
+    
+        for i in xrange(c['level'] + 1, 4):
+            ids[i] = 0
+    
+        if table and table.get(c['id']):
+            print(c['name'], table[c['id']])
+            record += Markers[c['level']] + c['name']
+            ids[c['level']] += 1            
+            
+            table[ids[c['level']]] = table[c['id']]
+            del table[c['id']]
+            
+    return record
+  
+  
+# Formatting
+def Format(table, level):
+    keys = set(range(len(Alphabet)) + table.keys())
+    level += 1
+    items = ''
+
+    for i in keys:
+        value = table.get(i)        
+        if value:
+            items += isinstance(i, int) and i > 0 and (Alphabet[i] + Markers[level]) or ''
+            items += FormatValue(value, level)
+        
+    return items
+    
+def FormatValue(value, level):
+    if isinstance(value, dict):
+        return Format(value, level)
+    elif isinstance(value, basestring):
+        return value
+    else:
+        return ''
+
+def CompressInt(v, size = 1):
+    v = int(v)
+    result = ''
+ 
+    while v != 0:
+        v, i = divmod(v, len(Alphabet))
+        result = Alphabet[i] + result
+        
+    return Alphabet[0] * (size - len(result)) + result
--- crawler.py
+++ crawler.py
@@ -1,134 +1,16 @@
 # -*- coding: utf-8 -*-
-import re, time
+import time
 from api import *
 
-# Localizations
-TargetFile = 'new_data.lua'
-ItemSearchURL = 'http://www.wowhead.com/items?filter=cr=151:151;crs=4:1;crv='
-ClassSearchURL = 'http://static.wowhead.com/js/locale_enus.js?1241'
-
-# Values
-Markers = {1:'{', 2:'}', 3:'$', 4:'€', 5:'£'}
-NumQualities = 7
-MaxItems = 115000
-MinItems = 0
-ItemBump = 50
-Verbose = None
-
-# Globals
+# Browse
 Start = time.time()
-Cache = {}
-Dir = type(Cache)
-String = type('')
-
-# Browse Items
-value = MinItems
-numItems = 0
-items = ''
-
-while value < MaxItems:
-    upper = str(value + ItemBump - 1)
-    lower = str(value)
-
-    if Verbose:
-        print('')
-        
-    print('Searching from ' + lower + ' to ' + upper)
-    source = GetWebPage(ItemSearchURL + upper + ':' + lower)
-    newItems = 0
-    
-    for match in re.finditer('"classs".*?cost', source):
-        full = match.group(0)
-        newItems += 1
-
-        itemID = GetNumber('"id', full)
-        name = re.sub('\\\\', '', GetValue('name', '"\d(.+?[^\\\])"', full))
-        itemData = itemID + ';' + name + ';'
-
-        itemClass = GetNumber('classs', full) or 'None'
-        subClass = GetNumber('subclass', full) or 'None'
-        slot = GetNumber('slot', full) or 'None'
-
-        quality = NumQualities - int(GetValue('name', '["\'](\d)', full))
-        level = GetNumber('reqlevel', full)
-        level = level and int(level) or 'None'
-
-        if Verbose:
-            print(itemData, level)
-            
-        SetTable(Cache, itemClass)
-        SetTable(Cache[itemClass], subClass)
-        SetTable(Cache[itemClass][subClass], slot)
-        AddItem(Cache[itemClass][subClass][slot], quality, level, itemData)
-
-    value += ItemBump
-    numItems += newItems
-    print('Found ' + str(newItems) + ' items (' + str(numItems) + ' total)')
-
-
-# Browse Categories
-print('')
-ids = [None, 0, 0, 0]
-classes = ''
-
-for c in GetClasses(ClassSearchURL):
-    parentCache = Cache
-
-    for i in range(1, c['level']):
-        parentCache = parentCache.get(ids[i])
-
-        if not parentCache:
-            break
-
-    for i in range(c['level'] + 1, 4):
-        ids[i] = 0
-
-    if parentCache and parentCache.get(c['id']):
-        newID = ids[c['level']] + 1
-        ids[c['level']] = newID
-        
-        classes = classes + Markers[c['level']] + c['name']
-        parentCache[newID] = parentCache[c['id']]
-        parentCache[c['id']] = None
-
-
-# Extract Cache
-def ExtractValue(table, level, marker, i):
-    global items
-    value = table.get(i)
-    
-    if value and not value == None:
-        if not type(i) == String:
-            items += str(i) + marker
-
-        if type(value) == Dir:
-            Extract(value, level)
-        else:
-            items += value.encode("utf-8")
-
-def Extract(table, level):
-    marker = Markers[level]
-    level = level + 1
-    done = {}
-    i = 1
-
-    while table.get(i):
-        ExtractValue(table, level, marker, i)
-        done[i] = True
-        i = i + 1
-        
-    for i, value in table.items():
-        if not done.get(i):
-            ExtractValue(table, level, marker, i)
-
-print('')
-print('Extracting Cache...')
-Extract(Cache, 1)
-
+TargetFile = 'new_data.lua'
+Classes = GetClasses('http://static.wowhead.com/js/locale_enus.js?1241')
+Items = Hierarchize(GetItems('http://www.wowhead.com/items?filter=cr=151:151;crs=4:1;crv=', range(0, 115001, 50)))
 
 # Write the File
 f = open(TargetFile, 'w')
-f.write('Ludwig_Classes="' + classes.encode("utf-8") + '"\nLudwig_Items=[['+ items + ']]')
+f.write('Ludwig_Classes="' + GenerateClassIDs(Classes, Items) + '"\nLudwig_Items=[['+ Format(Items, 0) + ']]')
 f.close()
 
 took = time.time() - Start
