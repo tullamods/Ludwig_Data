e7febe615b74c41797363e2dfa776f9fb1086708
Joao Cardoso <contact@jaliborc.com>
2017-05-02 01:38:34 +0100

    New database system and corresponding API working

--- api.lua
+++ api.lua
@@ -2,154 +2,93 @@
 	API for searching the item database
 
 	Usage:
-		:GetItems(name, category, quality, minLevel, maxLevel)
-		:GetItem(data, index)
-		:GetClosestItem(search)
+		:FindItems(search, class, subclass, quality, minLevel, maxLevel)
+		:FindClosestItem(search)
+		:ItemClassExists(class, subclass)
 		:GetItemLink(id, name, quality)
 --]]
 
-local ItemDB = Ludwig:NewModule('ItemDB')
-
-local MARKERS = {'$', '^', '*', '<', '´'}
-local LEVEL_MATCH = '(%d+)%' .. MARKERS[4] .. '([^%' .. MARKERS[4] .. ']+)'
-local QUALITY_MATCH = '(%d+)%' .. MARKERS[5]
-local ITEM_MATCH = '(.-)(%w%w%w%w)([^_]+)'
-
-local function newCache()
-	local t = {}
-	for i = 0, #ITEM_QUALITY_COLORS do
-		t[tostring(i)] = {}
-	end
-	return t
-end
-
-local function improveCache(table)
-	for i, v in pairs(table) do
-		table[tonumber(i)] = v
-	end
-	return table
-end
+local Database = Ludwig:NewModule('Database')
+local ITEM_MATCH = '(%w%w%w%w)([^_]+)'
 
 
 --[[ Searches ]]--
 
-function ItemDB:GetItems(name, class, minLevel, maxLevel, quality)
-	local quality = quality and tostring(quality)
-	local search = name and {strsplit(' ', name:lower())}
-	local ids, names, limits = newCache(), newCache(), {}
-	local data, list, numResults = Ludwig_Items, {}, 0
+function Database:FindItems(search, class, subclass, quality, minLevel, maxLevel)
+	local ids, names = {}, {}
 
-	-- Class
-	if class then
-		local match = ''
-		for i, value in ipairs(class) do
-			match = match .. tostring(value) .. '%' .. MARKERS[i] .. '.-'
-		end
+	search = search and search:lower()
+	maxLevel = maxLevel or math.huge
+	minLevel = minLevel or 0
 
-		match = match:sub(1, -3) .. '([^%' .. MARKERS[#class] .. ']+)'
-		data = data:match(match)
-	end
+	for category, subclasses in pairs(Ludwig_Items) do
+		if not class or class == category then
+			for subcat, qualities in pairs(subclasses) do
+				if not subclass or subclass == subcat then
+					for rarity, levels in pairs(qualities) do
+						if not quality or quality == rarity then
+							ids[rarity] = ids[rarity] or {}
+							names[rarity] = names[rarity] or {}
 
-	-- Level
-	if minLevel or maxLevel then
-		minLevel = tonumber(minLevel or 0)
-		maxLevel =  tonumber(maxLevel or 1000)
-		local results = ''
-
-		for level, items in data:gmatch(LEVEL_MATCH) do
-			level = tonumber(level)
-			if level >= minLevel and level <= maxLevel then
-				tinsert(list, items)
-			end
-		end
-	else
-		tinsert(list, data)
-	end
-
-	for _, items in ipairs(list) do
-		for extra, id, name in items:gmatch(ITEM_MATCH) do
-			-- Quality
-			local q = extra:match(QUALITY_MATCH)
-			if q then
-				if not quality or q == quality then
-					qualityNames = names[q]
-					qualityIDs = ids[q]
-				else
-					qualityIDs = nil
-				end
-			end
-
-			-- Name
-			if qualityIDs then
-				local match = true
-
-				if search then
-					local name = name:lower()
-
-					for i, word in ipairs(search) do
-						if not name:match(word) then
-							match = nil
-							break
+							for level, items in pairs(levels) do
+								if level >= minLevel and level <= maxLevel then
+									for id, name in items:gmatch(ITEM_MATCH) do
+										if not search or name:lower():find(search) then
+											tinsert(ids[rarity], id)
+											tinsert(names[rarity], name)
+										end
+									end
+								end
+							end
 						end
 					end
 				end
-
-				if match then
-					tinsert(qualityIDs, id)
-					tinsert(qualityNames, name)
-				end
 			end
 		end
 	end
 
-	-- Calculations
-	improveCache(ids)
-	improveCache(names)
-
-	for i = 0, #ITEM_QUALITY_COLORS do
-		numResults = numResults + #ids[i]
-		limits[i] = numResults
-	end
-
-	return {ids, names, limits}, numResults
+	return ids, names
 end
 
-function ItemDB:GetClosestItem(search)
+function Database:FindClosestItem(search)
 	local size = #search
-	local search = '^' .. search
+	local search = '^' .. search:lower()
 	local distance = math.huge
 	local bestID, bestName, bestQuality
 
-	for extra, id, name in Ludwig_Items:gmatch(ITEM_MATCH) do
-		quality = extra:match(QUALITY_MATCH) or quality
-
-		if name:match(search) then
-			local off = #name - size
-			if off >= 0 and off < distance then
-				bestID, bestName, bestQuality = id, name, quality
-				distance = off
+	for class, subclasses in pairs(Ludwig_Items) do
+		for subclass, qualities in pairs(subclasses) do
+			for quality, levels in pairs(qualities) do
+				for level, items in pairs(levels) do
+					for id, name in items:gmatch(ITEM_MATCH) do
+						if name:lower():match(search) then
+							local off = #name - size
+							if off >= 0 and off < distance then
+								bestID, bestName, bestQuality = id, name, quality
+								distance = off
+							end
+						end
+					end
+				end
 			end
 		end
 	end
 
 	if bestID then
-		return tonumber(bestID, 36), bestName, tonumber(bestQuality)
+		return tonumber(bestID, 36), bestName, bestQuality
 	end
 end
 
-
---[[ Items ]]--
-
-function ItemDB:GetItem(data, index)
-	local ids, names, limits = unpack(data)
-	for q = 0, #ITEM_QUALITY_COLORS do
-		if limits[q] >= index then
-			index = index - (limits[q - 1] or 0)
-			return tonumber(ids[q][index], 36), names[q][index], q
-		end
+function Database:ItemClassExists(class, subclass)
+	if subclass then
+		return Ludwig_Items[class] and Ludwig_Items[class][subclass]
 	end
+	return Ludwig_Items[class]
 end
 
-function ItemDB:GetItemLink(id, name, quality)
+
+--[[ Utilities ]]--
+
+function Database:GetItemLink(id, name, quality)
 	return ('%s|Hitem:%d:::::::::::::::|h[%s]|h|r'):format(ITEM_QUALITY_COLORS[quality].hex, id, name)
 end
