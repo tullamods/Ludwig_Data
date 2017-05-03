3ae8db86db72cde9baf9eeef154364a7e8e8ce11
Joao Cardoso <contact@jaliborc.com>
2017-05-03 19:21:28 +0100

    Working with new Battle.net based database

--- api.lua
+++ api.lua
@@ -2,9 +2,9 @@
 	API for searching the item database
 
 	Usage:
-		:FindItems(search, class, subclass, quality, minLevel, maxLevel)
-		:FindClosestItem(search)
-		:ItemClassExists(class, subclass)
+		:Find([search, class, subclass, slot, quality, minLevel, maxLevel])
+		:FindClosest(search)
+		:ClassExists(class [, subclass, slot])
 		:GetItemLink(id, name, quality)
 --]]
 
@@ -14,7 +14,7 @@ local ITEM_MATCH = '(%w%w%w%w)([^_]+)'
 
 --[[ Searches ]]--
 
-function Database:FindItems(search, class, subclass, quality, minLevel, maxLevel)
+function Database:Find(search, class, subclass, slot, quality, minLevel, maxLevel)
 	local ids, names = {}, {}
 
 	search = search and search:lower()
@@ -23,19 +23,23 @@ function Database:FindItems(search, class, subclass, quality, minLevel, maxLevel
 
 	for category, subclasses in pairs(Ludwig_Items) do
 		if not class or class == category then
-			for subcat, qualities in pairs(subclasses) do
+			for subcat, slots in pairs(subclasses) do
 				if not subclass or subclass == subcat then
-					for rarity, levels in pairs(qualities) do
-						if not quality or quality == rarity then
-							ids[rarity] = ids[rarity] or {}
-							names[rarity] = names[rarity] or {}
+					for equipSlot, qualities in pairs(slots) do
+						if not slot or slot == equipSlot then
+							for rarity, levels in pairs(qualities) do
+								if not quality or quality == rarity then
+									ids[rarity] = ids[rarity] or {}
+									names[rarity] = names[rarity] or {}
 
-							for level, items in pairs(levels) do
-								if level >= minLevel and level <= maxLevel then
-									for id, name in items:gmatch(ITEM_MATCH) do
-										if not search or name:lower():find(search) then
-											tinsert(ids[rarity], id)
-											tinsert(names[rarity], name)
+									for level, items in pairs(levels) do
+										if level >= minLevel and level <= maxLevel then
+											for id, name in items:gmatch(ITEM_MATCH) do
+												if not search or name:lower():find(search) then
+													tinsert(ids[rarity], id)
+													tinsert(names[rarity], name)
+												end
+											end
 										end
 									end
 								end
@@ -50,22 +54,24 @@ function Database:FindItems(search, class, subclass, quality, minLevel, maxLevel
 	return ids, names
 end
 
-function Database:FindClosestItem(search)
+function Database:FindClosest(search)
 	local size = #search
 	local search = '^' .. search:lower()
 	local distance = math.huge
 	local bestID, bestName, bestQuality
 
 	for class, subclasses in pairs(Ludwig_Items) do
-		for subclass, qualities in pairs(subclasses) do
-			for quality, levels in pairs(qualities) do
-				for level, items in pairs(levels) do
-					for id, name in items:gmatch(ITEM_MATCH) do
-						if name:lower():match(search) then
-							local off = #name - size
-							if off >= 0 and off < distance then
-								bestID, bestName, bestQuality = id, name, quality
-								distance = off
+		for subclass, slots in pairs(subclasses) do
+			for equipSlot, qualities in pairs(slots) do
+				for quality, levels in pairs(qualities) do
+					for level, items in pairs(levels) do
+						for id, name in items:gmatch(ITEM_MATCH) do
+							if name:lower():match(search) then
+								local off = #name - size
+								if off >= 0 and off < distance then
+									bestID, bestName, bestQuality = id, name, quality
+									distance = off
+								end
 							end
 						end
 					end
@@ -79,16 +85,27 @@ function Database:FindClosestItem(search)
 	end
 end
 
-function Database:ItemClassExists(class, subclass)
-	if subclass then
+function Database:ClassExists(class, subclass, slot)
+	if slot then
+		return Ludwig_Items[class] and Ludwig_Items[class][subclass] and Ludwig_Items[class][subclass][slot]
+	elseif subclass then
 		return Ludwig_Items[class] and Ludwig_Items[class][subclass]
+	else
+		return Ludwig_Items[class]
 	end
-	return Ludwig_Items[class]
+end
+
+function Database:HasEquipSlots(class, subclass)
+	for slot in pairs(Ludwig_Items[class][subclass]) do
+		if slot ~= 0 then
+			return true
+		end
+	end
 end
 
 
 --[[ Utilities ]]--
 
-function Database:GetItemLink(id, name, quality)
+function Database:GetLink(id, name, quality)
 	return ('%s|Hitem:%d:::::::::::::::|h[%s]|h|r'):format(ITEM_QUALITY_COLORS[quality].hex, id, name)
 end
